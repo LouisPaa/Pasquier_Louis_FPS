bf95299fdc5703b4a92b8ff9371249be81011435
PasquierLouis <l.pasquier@bdx.ecolescreatives.com>
2026-01-19 18:01:05 +0100

    Update CameraController.cs

--- Mon FPS/Assets/CameraController.cs
+++ Mon FPS/Assets/CameraController.cs
@@ -7,13 +7,13 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      Cursor.lockState = CursorLockMode.Locked;
+      Cursor.lockState = CursorLockMode.Locked; // Verrouille le curseur au centre de l'écran
     }
 
     // Update is called once per frame
     void Update()
     {
-        float yRotation = Input.GetAxis("Mouse Y") * mouseY * Time.deltaTime;
-        transform.Rotate(-yRotation, 0f, 0f);
+        float yRotation = Input.GetAxis("Mouse Y") * mouseY * Time.deltaTime; // Récupère le mouvement de la souris sur l'axe Y 
+        transform.Rotate(-yRotation, 0f, 0f); // Fait tourner la caméra en fonction du mouvement de la souris
     }
 }
